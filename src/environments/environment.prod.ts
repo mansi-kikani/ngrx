4eac9eb8a3655c2aca55c48b1224346f767e7976
mansi-kikani <mansik.tagline@gmail.com>
2022-06-17 17:37:28 +0530

    update prod env file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  API_URL: 'https://jsonplaceholder.typicode.com/posts',
+  FIRBASE_API_KEY:'AIzaSyA9HEOZrRHZP026VQObeDz2PVD_GLpMV50'
 };
